262cbbf6a067ad6b71bce67b3c5ecaa27923e82e
LindaJT <jokinenlindak@gmail.com>
2021-04-01 13:38:50 +0300

    broken access control

--- security_project/polls/urls.py
+++ security_project/polls/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
-   ## path('logout/', views.logout, name='logout'),
 ]
